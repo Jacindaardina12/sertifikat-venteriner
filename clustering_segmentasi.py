2f3160e92a66fe3a5a8466f2e2d96757afc72e7c
Jacindaardina12 <jacindaardinagestyaki@gmail.com>
2025-12-21 20:28:33 +0700

    cluster

--- clustering_segmentasi.py
+++ clustering_segmentasi.py
@@ -3,17 +3,20 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-def kmeans_segmentasi(df, n_clusters=3):
-    """
-    Melakukan clustering K-Means
-    """
-    fitur = df.select_dtypes(include='number')
 
-
+def scaling_data(df, fitur):
+    """
+    Melakukan scaling data numerik
+    """
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(fitur)
+    X_scaled = scaler.fit_transform(df[fitur])
+    return X_scaled, scaler
 
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    df['cluster'] = kmeans.fit_predict(X_scaled)
 
-    return df
+def train_kmeans(X_scaled, n_clusters):
+    """
+    Melatih model K-Means
+    """
+    model = KMeans(n_clusters=n_clusters, random_state=42)
+    labels = model.fit_predict(X_scaled)
+    return model, labels
